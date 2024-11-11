a76cb2bb60fca557db8cb9f91fcf93dccc04680a
Bhukubabu <143258170+bhukubabu@users.noreply.github.com>
2024-11-11 17:27:36 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,55 @@
+# Import the following module 
+from email.mime.text import MIMEText 
+from email.mime.image import MIMEImage 
+from email.mime.application import MIMEApplication 
+from email.mime.multipart import MIMEMultipart 
+import smtplib 
+import os 
+def start():
+	app_pass=os.getenv("app_password")
+
+	smtp = smtplib.SMTP('smtp.gmail.com', 587) 
+	smtp.ehlo() 
+	smtp.starttls() 
+
+	smtp.login('bhuktajaya@gmail.com', app_pass) 
+
+	def message(subject="Emergency alert", 
+				text="", img=None, 
+				attachment=None): 
+		
+		msg = MIMEMultipart() 
+		msg['Subject'] = subject 
+		msg.attach(MIMEText(text)) 
+		
+		if img is not None: 
+			if type(img) is not list:  
+				img = [img] 
+
+			# Now iterate through our list 
+			for one_img in img: 
+				
+				# read the image binary data 
+				img_data = open(one_img, 'rb').read() 
+				msg.attach(MIMEImage(img_data, 
+									name=os.path.basename(one_img))) 
+		return msg 
+
+	msg = message(
+		"Emergency alert",
+		'''Respected user,
+		A nearby user is at risk. Please help him. 
+		
+	This message is sent from the Semi;colon team as part of the emergency response.
+		''',
+		img=r"emergency-alert.jpg"
+		) 
+
+	to = ["jayabhukta@gmail.com","avneetdas10@gmail.com","akashcollegeworks@gmail.com"] 
+	smtp.sendmail(from_addr="bhuktajaya@gmail.com", 
+				to_addrs=to, msg=msg.as_string()) 
+
+	smtp.quit()
+	
+if __name__=="__main__":
+	start()
---
+++ sos.py
@@ -0,0 +1,23 @@
+import json 
+import requests 
+
+import time
+import requests
+import subprocess
+import streamlit as st
+from streamlit_lottie import st_lottie_spinner
+
+st.title("ALERT !🚨")
+def load_lottiefile(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+lottie = load_lottiefile("Animation - 1731325019108.json")
+c=True
+while True:
+    with st_lottie_spinner(lottie, loop=True,width =300):
+        time.sleep(1)
+        st.write("Request is sending....")
+        subprocess.run(['python','mail.py'])
+        break
+st.success("Message sent successfully")
+
