939e4df12f97dbbafecec01bc72fab159d80c79e
Bhukubabu <143258170+bhukubabu@users.noreply.github.com>
2024-11-11 18:09:15 +0530

    Update sos.py

--- sos.py
+++ sos.py
@@ -14,11 +14,11 @@ def load_lottiefile(filepath: str):
 lottie = load_lottiefile("Animation - 1731325019108.json")
 c=True
 while True:
-    with st_lottie_spinner(lottie, loop=True,width =300):
+    with st_lottie_spinner(lottie, loop=True,width =400):
         time.sleep(10)
-    st.write("Request is sending....")
-    subprocess.run(['python','mail.py'])
-    st.success("Message sent successfully")
+        st.write("Request is sending....")
+        subprocess.run(['python','mail.py'])
+        st.success("Message sent successfully")
     
     break
 
