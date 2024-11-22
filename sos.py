ce3fe4e49861595f700fb8d235a8a16694432afd
Bhukubabu <143258170+bhukubabu@users.noreply.github.com>
2024-11-22 23:56:20 +0530

    Update sos.py

--- sos.py
+++ sos.py
@@ -13,18 +13,23 @@ st.title("ALERT !🚨")
 def load_lottiefile2(file_path:str):
       with open(file_path,"r") as file:
             return json.load(file)
+      
 def load_lottiefile1(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
 lottie = load_lottiefile1("Animation - 1731325019108.json")
 c=True
 #if not st.session_state.email_sent: 
-with st_lottie_spinner(lottie, loop=True,width =400):
+with st_lottie_spinner(lottie, loop=True,width =350):
           time.sleep(10)
           subprocess.run(["python","mail.py"])
           #st.session_state.email_sent=True
           #break 
-st.success("Message sent successfully")
+success_lottie=load_lottiefile2("Animation - 1732299766527.json")
+with st_lottie_spinner(success_lottie,loop=True,width=350):
+    time.sleep(4)
+    st.success("Message sent successfully")
 #else: 
 #    st.info("Email has sent already")
             #break
+
