1e025fb6476b69e78182647b9c04c334880048c5
Bhukubabu <143258170+bhukubabu@users.noreply.github.com>
2024-11-22 22:19:09 +0530

    Update sos.py

--- sos.py
+++ sos.py
@@ -16,12 +16,12 @@ def load_lottiefile(filepath: str):
 lottie = load_lottiefile("Animation - 1731325019108.json")
 c=True
 if not st.session_state.email_sent: 
-      with st_lottie_spinner(lottie, loop=True,width =400):
+    with st_lottie_spinner(lottie, loop=True,width =400):
           time.sleep(10)
           mail.start()
           st.session_state.email_sent=True
-          break 
-      st.success("Message sent successfully")
+          #break 
+    st.success("Message sent successfully")
 else: 
      st.info("Email has sent already")
             #break
