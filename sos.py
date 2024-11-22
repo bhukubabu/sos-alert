37bdfc3959d7701cf21b4966686f70067219dac0
Bhukubabu <143258170+bhukubabu@users.noreply.github.com>
2024-11-22 17:50:48 +0530

    Update sos.py

--- sos.py
+++ sos.py
@@ -7,18 +7,24 @@ import subprocess
 import streamlit as st
 from streamlit_lottie import st_lottie_spinner
 
+if "email_sent" not in st.session_state:
+    st.session_state.email_sent= False 
 st.title("ALERT !🚨")
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
 lottie = load_lottiefile("Animation - 1731325019108.json")
 c=True
-with st_lottie_spinner(lottie, loop=True,width =400):
-        time.sleep(10)
+if not st.session_state.email_sent: 
+      with st_lottie_spinner(lottie, loop=True,width =400):
+           time.sleep(10)
         #st.write("Request is sending....")
         #subprocess.run(['python','mail.py'])
-        mail.start()
+           mail.start()
+           st.session_state.email_sent=True
             #break 
-st.success("Message sent successfully")
+       st.success("Message sent successfully")
+else: 
+     st.info("Email has sent already")
             #break
 
